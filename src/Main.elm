4f73d68055d86add3a57f247110bca63074f5bf6
xenopol <x.enopol@yahoo.com>
2019-07-21 16:34:50 +0200

    Add pictures and CSS

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,31 @@
 module Main exposing (main)
 
-import Html
+import Html exposing (..)
+import Html.Attributes exposing (class, src)
 
 
+baseUrl : String
+baseUrl =
+    "https://programming-elm.com/"
+
+
+viewDetailedPhoto : String -> String -> Html msg
+viewDetailedPhoto url caption =
+    div [ class "detailed-photo" ]
+        [ img [ src url ] []
+        , div [ class "photo-info" ]
+            [ h2 [ class "caption" ] [ text caption ] ]
+        ]
+
+
+main : Html msg
 main =
-    Html.text "Hello world"
+    div []
+        [ div [ class "header" ]
+            [ h1 [] [ text "Picshare" ] ]
+        , div [ class "content-flow" ]
+            [ viewDetailedPhoto (baseUrl ++ "1.jpg") "Surfing"
+            , viewDetailedPhoto (baseUrl ++ "2.jpg") "The Fox"
+            , viewDetailedPhoto (baseUrl ++ "3.jpg") "Evening"
+            ]
+        ]
